77ccf6f23c702f7e12f1de8558b9719b25daa814
john vincent calma <vincent.calma@pathmazing.com>
2023-02-09 21:18:28 +0700

    feat(header): implement hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home(): JSX.Element {
-  return <div>hi</div>;
+  return (
+    <div>
+      <div className="hero center">
+        <div className="center w-full">
+          <div className="css-typing italic text-50 text-white-100 font-semibold text-center">
+            <p>Veni Vidi Vici</p>
+            <p>I came, I saw, I conquered.</p>
+            <p>Dumating ako, Nakita ko, Nagtagumpay ako.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
